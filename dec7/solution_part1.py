17104d0ac6f574e5b4f332a5f8e303e6413c52af
thatgeeman <thatgeeman@users.noreply.github.com>
2022-12-08 14:40:50 +0100

    add working solution

--- dec7/solution_part1.py
+++ dec7/solution_part1.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import re
+import joblib
 
 
 def read_file(filename):
@@ -20,18 +21,29 @@ def join(string):
 
 
 class File:
-    def __init__(self, name, type="file", size=0, is_dir=None, parent=None) -> None:
+    def __init__(
+        self,
+        name,
+        type="file",
+        size=0,
+        is_dir=None,
+        parent=None,
+        depth=None,
+        children=None,
+    ) -> None:
         self.name = name
         self.type = type
         self.size = size
         self.is_dir = is_dir
         self.parent = parent
+        self.depth = depth
+        self.children = children
 
     def __iter__(self):
         return iter([self])
 
     def __repr__(self) -> str:
-        return f"File({self.parent}, {self.name}, {self.type})"
+        return f"File(parent={self.parent}, name={self.name}, t={self.type}, s={self.size}, d={self.depth})"
 
     def __str__(self) -> str:
         return self.__repr__()
@@ -45,7 +57,6 @@ class System:
         self.counter = 0
         self.depth = 0
         self._prev_dirs = []
-        self._parent = None
 
     def run(self, command):
         command_arg = None
@@ -89,52 +100,76 @@ class System:
                 name = item[4:]
             else:
                 type = "file"
-                size = int(join(re.findall("[(0-9)+]", item)))
-                name = join(re.findall("[([aA-zZ)+.]", item))
+                size = int(re.findall("[(0-9)]+", item)[0])
+                # some files do not have .ext
+                name = re.findall("[a-zA-Z].+|[a-zA-Z]+", item)[0]
             file = File(
                 size=size,
                 name=name,
                 type=type,
                 is_dir=is_dir,
                 parent=self._prev_dirs[-1],
+                depth=self.depth,
             )
+            print(f"found {file} in {self._prev_dirs[-1]}")
             self.log[self.depth].append(file)
 
 
-class Tree:
-    def __init__(self, node, parent=None, children=[]) -> None:
-        self.node = node
-        self.parent = parent
-        self.children = children
+def build_flat_tree(log):
+    max_depth = max(log)  # key to access deepest node
+    tree = []
+    reverse_order = list(range(0, max_depth + 1))[::-1]
+    for depth in reverse_order:
+        parent = None
+        for contents in log[depth]:
+            if depth > 0:
+                # find parent
+                for prev_contents in log[depth - 1]:
+                    if prev_contents.name == contents.parent:
+                        parent = prev_contents
+                        break
+            contents.parent = parent
+            tree.append(contents)
+    return tree
 
-    def __repr__(self) -> str:
-        return f"Tree({self.parent}, {self.node}, {len(self.children)})"
 
-    def __str__(self) -> str:
-        return self.__repr__()
+def find_common_parent(t, other_paths):
+    """
+    Take a path `t` and compare iteratively with other paths `other_path` to
+    find if they are in the same directory (under the same parent).
+
+    Will return [`t`] if no other paths with same parent.
+    """
+    l = []
+    for op in other_paths:
+        if t.parent == None:
+            # if last node
+            continue
+        if t.parent != op.parent:
+            # if different parents
+            continue
+        l.append(op)
+    return l
 
 
-def build_tree(log):
-    tree = defaultdict(lambda: [])
-    for depth in log:
-        for item in log[depth]:
-            tree[f"{item.parent}"].append(item)
-    print(dict(tree))
-    # process directories again
-    for dir in tree.copy():
-        print(dir)
-        items = tree[dir]
-        tree[dir] = defaultdict(lambda: [])
-        for item in items:
-            if item.is_dir:
-                tree[dir][f"{item.name}"].append(tree[f"{item.name}"])
-                tree.pop(f"{item.name}")
-            else:
-                tree[dir][f"{item.name}"].append(item)
-
-        tree[dir] = dict(tree[dir])
-    print(f"complete")
-    return dict(tree)
+def assign_dir_size(t, other_paths):
+    """
+    Find parent directories and assign the size attribute of
+    each parent, starting from the deepest File (see `build_flat_tree`).
+    """
+    siblings = find_common_parent(t, other_paths)
+    print(f"sib: {len(siblings)}, d={t.depth}, name={t.name}", end=" ")
+    dir_size = 0
+    for s in siblings:
+        # if s.size <= 100000:
+        # assign all sizes, filter later
+        dir_size += s.size
+    if (t.parent) and (t.parent.size == 0):
+        # only assign oif not already assigned by other paths.
+        print(f"assigned {t.parent.size} -> {dir_size}")
+        t.parent.size = dir_size
+    elif t.parent:
+        print(f"not assigned current: {t.parent.size} suggested add: {dir_size}")
 
 
 if __name__ == "__main__":
@@ -145,12 +180,27 @@ if __name__ == "__main__":
     os = System(console_log)
     for c in console_log:
         os.run(c)
-    # print(os.counter)
-    print(f"full tree: {dict(os.log)}")
-    print(f"full tree: {os.log.keys()}")
-    print(f"full tree-x: {[n.name for n in os.log[1]]}")
-    print(f"full tree-x: {[n.parent for n in os.log[1]]}")
+    # save
+    tmp_log = os.log.copy()
+    root_folder = File(parent=None, name="/", size=0, depth=0, type="dir", is_dir=True)
+    tmp_log[0] = [root_folder]
+    joblib.dump(dict(tmp_log), "dec7/inp.joblib")
+    tmp_log = joblib.load("dec7/inp.joblib")
     # print(f"\nbuilding tree")
-    tree = build_tree(dict(os.log))
-    print(tree.keys())
-    print(tree)
+    print(tmp_log)
+    tree = build_flat_tree(tmp_log)
+
+    for t in tree:
+        assign_dir_size(t, tree)
+
+    # taker sum
+    sum_lt = 0
+    for t in tree:
+        for p in set(os._prev_dirs):
+            if t.parent is None:
+                continue
+            if (t.parent.name == p) and (t.size <= 100000) and (t.is_dir):
+                # print(t.size)
+                sum_lt += t.size
+
+    print(f"\n\nsum is {sum_lt}")
